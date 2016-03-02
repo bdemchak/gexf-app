b4c119ee0541def69ab49a49e67b14c2c3e35328
Jeremy <jeremy.mcelhatten@gmail.com>
2016-03-01 21:13:54 -0500

    Added data types from GEXF 1.3

--- src/main/java/edu/umuc/swen670/gexf/internal/model/DataTypes.java
+++ src/main/java/edu/umuc/swen670/gexf/internal/model/DataTypes.java
@@ -3,10 +3,16 @@ package edu.umuc.swen670.gexf.internal.model;
 public final class DataTypes {
 
 	public static final String INTEGER = "integer";
-	public static final String DOUBLE = "double";
+	public static final String LONG = "long";
 	public static final String FLOAT = "float";
+	public static final String DOUBLE = "double";
 	public static final String BOOLEAN = "boolean";
 	public static final String STRING = "string";
+	public static final String LISTINTEGER = "listinteger";
+	public static final String LISTLONG = "listlong";
+	public static final String LISTFLOAT = "listfloat";
+	public static final String LISTDOUBLE = "listdouble";
+	public static final String LISTBOOLEAN = "listboolean";
 	public static final String LISTSTRING = "liststring";
 	
 	private DataTypes(){}
