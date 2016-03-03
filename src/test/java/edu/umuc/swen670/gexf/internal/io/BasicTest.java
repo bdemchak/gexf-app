a59febbf5fe8f4a93482e6a07b1ea22f56544557
Jeremy <jeremy.mcelhatten@gmail.com>
2016-03-02 22:07:16 -0500

    Added edge checking to BasicTest

--- src/test/java/edu/umuc/swen670/gexf/internal/io/BasicTest.java
+++ src/test/java/edu/umuc/swen670/gexf/internal/io/BasicTest.java
@@ -2,6 +2,9 @@ package edu.umuc.swen670.gexf.internal.io;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import org.junit.After;
@@ -18,10 +21,13 @@ import static org.mockito.Mockito.mock;
 import org.cytoscape.ding.NetworkViewTestSupport;
 import org.cytoscape.io.read.CyNetworkReader;
 import org.cytoscape.model.CyColumn;
+import org.cytoscape.model.CyEdge;
+import org.cytoscape.model.CyIdentifiable;
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNetworkFactory;
 import org.cytoscape.model.CyNetworkManager;
 import org.cytoscape.model.CyNode;
+import org.cytoscape.model.CyRow;
 import org.cytoscape.model.CyTable;
 import org.cytoscape.model.NetworkTestSupport;
 import org.cytoscape.model.subnetwork.CyRootNetworkManager;
@@ -49,15 +55,31 @@ public class BasicTest {
 		assertEquals(2, cyNetwork.getNodeCount());
 		assertEquals(1, cyNetwork.getEdgeCount());
 		
+		
+		
+		//build the node map
+		HashMap<String, Long> nodeNameId = new HashMap<String, Long>();
+		CyTable cyNodeTable = cyNetwork.getDefaultNodeTable();
+		List<CyRow> cyNodeRows = cyNodeTable.getAllRows();
+		for(CyRow cyNodeRow : cyNodeRows) {
+			nodeNameId.put(cyNodeRow.get("name", String.class), cyNodeRow.get("SUID", Long.class));
+		}
+		
 		//check the nodes
-		CyTable cyTable = cyNetwork.getDefaultNodeTable();
-		CyColumn nameColumn = cyTable.getColumn("name");
-		List<String> names = nameColumn.getValues(String.class);
-		assertEquals(true, names.contains("Hello"));
-		assertEquals(true, names.contains("Word"));
+		assertEquals(true, nodeNameId.containsKey("Hello"));
+		assertEquals(true, nodeNameId.containsKey("World"));
+		
+		
+		
+		//build the edge map
+		HashMap<Long, List<Long>> edgeMapping = new HashMap<Long, List<Long>>();
+		edgeMapping.put(nodeNameId.get("Hello"), Arrays.asList(nodeNameId.get("World")));
 		
 		//check the edges
-		//???
+		List<CyEdge> cyEdges = cyNetwork.getEdgeList();
+		for(CyEdge cyEdge : cyEdges) {
+			assertEquals(true, edgeMapping.get(cyEdge.getSource().getSUID()).contains(cyEdge.getTarget().getSUID()));
+		}
 	}
 	
 	private CyNetwork[] RunFile(InputStream stream) throws Exception {
