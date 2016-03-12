1174c14ad941818c0b55b11350dc857185f0e47e
Jeremy <jeremy.mcelhatten@gmail.com>
2016-03-12 16:05:09 -0500

    Fix error with edge type value

--- src/main/java/edu/umuc/swen670/gexf/internal/model/GEXFEdge.java
+++ src/main/java/edu/umuc/swen670/gexf/internal/model/GEXFEdge.java
@@ -5,7 +5,7 @@ public final class GEXFEdge {
 	public static final String ID = "id";
 	public static final String SOURCE = "source";
 	public static final String TARGET = "target";
-	public static final String EDGETYPE = "edgetype";
+	public static final String EDGETYPE = "type";
 	public static final String WEIGHT = "weight";
 	
 	private GEXFEdge(){}
