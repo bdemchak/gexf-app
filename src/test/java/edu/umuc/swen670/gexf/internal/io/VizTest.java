c60e39a6986b02bb430cf44f84b4d2557c0c86e6
Jeremy <jeremy.mcelhatten@gmail.com>
2016-03-16 23:06:29 -0400

    Created test for viz12.gexf

---
+++ src/test/java/edu/umuc/swen670/gexf/internal/io/VizTest.java
@@ -0,0 +1,40 @@
+package edu.umuc.swen670.gexf.internal.io;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.cytoscape.model.CyNetwork;
+import org.junit.Test;
+
+public class VizTest extends TestBase {
+
+	@Test
+	public void ParseVizFile() throws Exception {
+		InputStream stream = this.getClass().getClassLoader().getResourceAsStream("testData/gexf/viz12.gexf");
+		assertNotNull(stream);
+		
+		CyNetwork[] cyNetworks = RunFile(stream);
+		
+		assertNotNull(cyNetworks);
+		assertEquals(1, cyNetworks.length);
+		
+		CyNetwork cyNetwork = cyNetworks[0];
+		
+		//check the counts
+		assertEquals(1, cyNetwork.getNodeCount());
+		
+		
+		//build the node map
+		HashMap<String, Long> nodeNameId = BuildNodeMap(cyNetwork);
+		
+		//check the nodes
+		assertEquals(true, nodeNameId.containsKey("glossy"));
+	}
+
+}
