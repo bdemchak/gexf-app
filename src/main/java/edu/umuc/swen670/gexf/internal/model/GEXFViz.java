03e0f70cc2090949cead9a0c903da876c0545e68
Jeremy <jeremy.mcelhatten@gmail.com>
2016-03-16 21:31:11 -0400

    Added visualization constants

---
+++ src/main/java/edu/umuc/swen670/gexf/internal/model/GEXFViz.java
@@ -0,0 +1,32 @@
+package edu.umuc.swen670.gexf.internal.model;
+
+public final class GEXFViz {
+
+	public static final String VIZ = "viz";
+	
+	public static final String COLOR = "color";
+	public static final String POSITION = "position";
+	public static final String SIZE = "size";
+	public static final String SHAPE = "shape";
+	public static final String THICKNESS = "thickness";
+	
+	public static final String RED = "r";
+	public static final String GREEN = "g";
+	public static final String BLUE = "b";
+	public static final String ALPHA = "a";
+
+	public static final String X = "x";
+	public static final String Y = "y";
+	public static final String Z = "z";
+	
+	public static final String DISC = "disc";
+	public static final String SQUARE = "square";
+	public static final String TRIANGLE = "triangle";
+	public static final String DIAMOND = "diamond";
+	public static final String IMAGE = "image";
+	
+	public static final String SOLID = "solid";
+	public static final String DOTTED = "dotted";
+	public static final String DASHED = "dashed";
+	public static final String DOUBLE = "double";
+}
